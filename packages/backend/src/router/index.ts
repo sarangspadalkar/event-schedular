cd7349137e13d1c79f6c2bae1ca4d344fa51b478
Sarang Padalkar <padalkar.sarang@gmail.com>
2024-06-23 18:19:09 -0500

    rearrange files, and formatting

--- packages/backend/src/router/index.ts
+++ packages/backend/src/router/index.ts
@@ -1,11 +1,11 @@
-import express, { Response } from "express";
+import express, {Response} from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (res: Response) => {
-  res.send("Hello World!");
+app.get('/', (res: Response) => {
+    res.send('Hello World!');
 });
 app.listen(port, () => {
-  console.info(`Server running at http://localhost:${port}`);
+    console.info(`Server running at http://localhost:${port}`);
 });
